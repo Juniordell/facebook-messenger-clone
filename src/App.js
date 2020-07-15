ab22d441ab0f12a92b52842e145661ed161ac029
Nelson Jr <juniordell123@gmail.com>
2020-07-15 12:25:37 -0300

    Deploying and updating the style!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core/';
+import { FormControl, Input } from '@material-ui/core/';
 import './App.css';
 import Message from './Message';
 import db from './firebase'
 import firebase from 'firebase'
 import FlipMove from 'react-flip-move'
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 
 function App() {
   const [ input, setInput ] = useState('')
   const [ messages, setMessages ] = useState([{username: 'sonny', message: 'hey, whats up?'}])
   const [ username, setUsername ] = useState('')
   
-  console.log(messages)
-
   useEffect(() => {
     db.collection('messages')
     .orderBy('timestamp', "desc")
@@ -40,21 +40,25 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Clever Programmer</h1>
+      <img src='https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100' alt='logo'></img>
+      <h1>Messenger</h1>
       <h2>Welcome {username}</h2>
 
-      <form>
+      <form className='app__form'>
 
-      <FormControl>
-        <InputLabel>Enter a message...</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)}/>
-        <Button disabled={!input} 
+      <FormControl className='app__formControl'>
+        <Input className='app__input' placeholder='Enter a message...' value={input} onChange={event => setInput(event.target.value)}/>
+
+        <IconButton 
+        className="app__iconButton"
+        disabled={!input} 
         variant='contained' 
         color='primary' 
         type='submit' 
         onClick={sendMessage}>
-          Send message
-        </Button>
+          <SendIcon />
+        </IconButton>
+
       </FormControl>
 
       </form>
