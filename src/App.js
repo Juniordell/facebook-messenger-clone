be2049c431a7ccc2676db7bea150e9e5737d98cf
Nelson Jr <juniordell123@gmail.com>
2020-07-14 21:33:21 -0300

    Add animation in the message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core/';
 import './App.css';
-import db from './firebase'
 import Message from './Message';
+import db from './firebase'
+import firebase from 'firebase'
+import FlipMove from 'react-flip-move'
 
 function App() {
   const [ input, setInput ] = useState('')
-  const [ messages, setMessages ] = useState([{username: 'sonny', text: 'hey, whats up?'}])
+  const [ messages, setMessages ] = useState([{username: 'sonny', message: 'hey, whats up?'}])
   const [ username, setUsername ] = useState('')
   
   console.log(messages)
 
   useEffect(() => {
-    db.collection('messages').onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
+    db.collection('messages')
+    .orderBy('timestamp', "desc")
+    .onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
     })
   }, [])
 
@@ -23,7 +27,14 @@ function App() {
 
   const sendMessage = event => {
     event.preventDefault()
-    setMessages([...messages, {username, text: input}])
+
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    setMessages([...messages, {username: username, message: input}])
     setInput('')
   }
 
@@ -37,16 +48,25 @@ function App() {
       <FormControl>
         <InputLabel>Enter a message...</InputLabel>
         <Input value={input} onChange={event => setInput(event.target.value)}/>
-        <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send message</Button>
+        <Button disabled={!input} 
+        variant='contained' 
+        color='primary' 
+        type='submit' 
+        onClick={sendMessage}>
+          Send message
+        </Button>
       </FormControl>
 
       </form>
 
-      {
-        messages.map(message => (
-          <Message username={username} message={message} />
-        ))
-      }
+      <FlipMove>
+        {
+          messages.map(({id, message}) => (
+            <Message key={id} username={username} message={message} />
+          ))
+        }
+      </FlipMove>
+
 
     </div>
   );
